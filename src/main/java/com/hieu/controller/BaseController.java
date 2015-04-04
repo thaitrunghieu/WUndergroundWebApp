b960a7613643017eb276b5820d352ee9addb353f
Hieu Thai <thaitrunghieu@gmail.com>
2015-04-03 22:55:42 -0500

    update view and controller to get response from view.

--- src/main/java/com/hieu/controller/BaseController.java
+++ src/main/java/com/hieu/controller/BaseController.java
@@ -3,9 +3,9 @@ package com.hieu.controller;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BaseController {
@@ -13,9 +13,18 @@ public class BaseController {
     private static final String VIEW_INDEX = "index";
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index() {
+    public String weather(ModelMap model) {
+
+        model.addAttribute("message", "Please enter your zip code");
 
         // Spring uses InternalResourceViewResolver and return back index.jsp
+        return VIEW_INDEX;
+    }
+
+    @RequestMapping(value = "/", params = {"zip"}, method = RequestMethod.GET)
+    public String weatherWithGivenZip(@RequestParam("zip")String zipCode, ModelMap model) {
+
+        model.addAttribute("message", "Weather data in your zip code is " + zipCode);
         return VIEW_INDEX;
     }
 }
--- src/main/webapp/WEB-INF/pages/index.jsp
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -1,5 +1,11 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h1>Weather Underground Web Application</h1>
+<h3>${message}</h3>
+<form method="get" action="">
+    ZipCode:
+    <input type="text" name="zip" />
+    <input type="submit" value="Submit">
+</form>
 </body>
 </html>
